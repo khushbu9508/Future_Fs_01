ad683ecec67625d27648e388722acbc029886fb5
khushbu singh <khushbusinghsk9504@gmail.com>
2026-02-28 13:29:31 +0530

    Updated server port configuration for Render deployment

--- server.js
+++ server.js
@@ -157,8 +157,8 @@ app.post("/contact", async (req, res) => {
 
 /* ================= Start Server ================= */
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-  console.log(`🚀 Server running at http://localhost:${PORT}`);
+  console.log(`🚀 Server running on port ${PORT}`);
 });
